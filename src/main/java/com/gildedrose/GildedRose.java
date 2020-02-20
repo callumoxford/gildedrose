76a995f87624b89a0ce5a13bbbf541e408ea1069
dsperry-ape09 <daniel.sperry+ape09@gmail.com>
2020-02-20 17:07:36 +0000

    Switched to Polymorphic update implementation

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,8 +1,7 @@
 package com.gildedrose;
 
 class GildedRose {
-    public static final int BACKSTAGE_PASS_THRESHOLD_1 = 10;
-    public static final int BACKSTAGE_PASS_THRESHOLD_2 = 5;
+
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -11,60 +10,9 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            updateItemQuality(item);
+           ItemUpdater.generateUpdater(item).update();
         }
     }
 
-    private void updateItemQuality(Item item) {
-        if (isSulfuras(item)) {
-            return;
-        }
-        item.sellIn = item.sellIn - 1;
 
-        if (isAgedBrie(item)) {
-            if (item.sellIn < 0) {
-                increaseQuality(item, 2);
-            } else {
-                increaseQuality(item, 1);
-            }
-        } else if (isBackstagePass(item)) {
-            if (item.sellIn >= 0) {
-                if (item.sellIn < BACKSTAGE_PASS_THRESHOLD_2) {
-                    increaseQuality(item, 3);
-                } else if (item.sellIn < BACKSTAGE_PASS_THRESHOLD_1) {
-                    increaseQuality(item, 2);
-                } else {
-                    increaseQuality(item, 1);
-                }
-            } else {
-                item.quality = 0;
-            }
-        } else {
-            if (item.sellIn < 0) {
-                decreaseQuality(item, 2);
-            } else {
-                decreaseQuality(item, 1);
-            }
-        }
-    }
-
-    private void decreaseQuality(Item item, int decrement) {
-        item.quality = Math.max(0, item.quality - decrement);
-    }
-
-    private void increaseQuality(Item item, int i) {
-        item.quality = Math.min(50, item.quality + i);
-    }
-
-    private boolean isSulfuras(Item item) {
-        return item.name.equals("Sulfuras, Hand of Ragnaros");
-    }
-
-    private boolean isBackstagePass(Item item) {
-        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
-    }
-
-    private boolean isAgedBrie(Item item) {
-        return item.name.equals("Aged Brie");
-    }
 }
---
+++ src/main/java/com/gildedrose/ItemUpdater.java
@@ -0,0 +1,116 @@
+package com.gildedrose;
+
+
+public abstract class ItemUpdater {
+    protected Item item;
+
+    public ItemUpdater(Item item) {
+        this.item = item;
+    }
+
+    public void update() {
+        item.sellIn = item.sellIn - 1;
+    }
+
+    protected void decreaseQuality(Item item, int decrement) {
+        item.quality = Math.max(0, item.quality - decrement);
+    }
+
+    protected void increaseQuality(Item item, int i) {
+        item.quality = Math.min(50, item.quality + i);
+    }
+
+    static boolean isSulfuras(Item item) {
+        return item.name.equals("Sulfuras, Hand of Ragnaros");
+    }
+
+    static boolean isBackstagePass(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
+    }
+
+    static boolean isAgedBrie(Item item) {
+        return item.name.equals("Aged Brie");
+    }
+
+
+    public static ItemUpdater generateUpdater(Item item) {
+        if (isSulfuras(item)) {
+            return new SulfurasUpdater(item);
+        }
+        if (isAgedBrie(item)) {
+            return new AgedBrieUpdater(item);
+        }
+        if (isBackstagePass(item)) {
+            return new BackstagePassUpdater(item);
+        }
+        return new RegularItemUpdater(item);
+    }
+}
+
+class RegularItemUpdater extends ItemUpdater {
+    public RegularItemUpdater(Item item) {
+        super(item);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        if (item.sellIn < 0) {
+            decreaseQuality(item, 2);
+        } else {
+            decreaseQuality(item, 1);
+        }
+    }
+}
+
+class AgedBrieUpdater extends ItemUpdater {
+    public AgedBrieUpdater(Item item) {
+        super(item);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        if (item.sellIn < 0) {
+            increaseQuality(item, 2);
+        } else {
+            increaseQuality(item, 1);
+        }
+    }
+}
+
+class SulfurasUpdater extends ItemUpdater {
+    public SulfurasUpdater(Item item) {
+        super(item);
+    }
+
+    @Override
+    public void update() {
+        //Never updates
+    }
+}
+
+class BackstagePassUpdater extends ItemUpdater {
+    public static final int BACKSTAGE_PASS_THRESHOLD_1 = 10;
+    public static final int BACKSTAGE_PASS_THRESHOLD_2 = 5;
+
+    public BackstagePassUpdater(Item item) {
+        super(item);
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        if (item.sellIn >= 0) {
+            if (item.sellIn < BACKSTAGE_PASS_THRESHOLD_2) {
+                increaseQuality(item, 3);
+            } else if (item.sellIn < BACKSTAGE_PASS_THRESHOLD_1) {
+                increaseQuality(item, 2);
+            } else {
+                increaseQuality(item, 1);
+            }
+        } else {
+            item.quality = 0;
+        }
+    }
+}
